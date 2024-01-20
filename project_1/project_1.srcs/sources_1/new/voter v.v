c036d426ebfa5fc22d52e97b8dbacd59b7a41b9b
Bitnoc12 <112693822+Bitnoc12@users.noreply.github.com>
2024-01-21 01:38:57 +0800

    Add files via upload

---
+++ project_1/project_1.srcs/sources_1/new/voter v.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/04/25 16:47:53
+// Design Name: 
+// Module Name: voter v
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module voter v(
+
+    );
+endmodule
---
+++ project_1/project_1.srcs/sources_1/new/voter.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+module voter(
+       input a,
+       input b,
+       input c,
+       output f
+
+    );
+wire ab,ac,bc;
+and and1(ab,a,b);
+and and2(ac,a,c);
+and and3(bc,b,c);
+    
+or or1(f,ab,ac,bc);
+    
+endmodule
